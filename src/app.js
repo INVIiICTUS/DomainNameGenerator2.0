4ca016a5bc0bb537648ee435874d3f5a6c14d2ac
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-05-11 19:01:38 +0000

    v.1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   function dominios() {
+//     let pronoun = ["the", "our", "it", "he", "she"];
+//     let adj = ["great", "big", "small", "dangerous", "blue", "pink"];
+//     let noun = ["jogger", "racoon", "dog", "driver", "comedian"];
+//     let fin = [".com", ".es", ".us", ".it"];
+
+//     let proIndex = Math.floor(Math.random() * pronoun.length);
+//     let adjIndex = Math.floor(Math.random() * adj.length);
+//     let nounIndex = Math.floor(Math.random() * noun.length);
+//     let finalIndex = Math.floor(Math.random() * fin.length);
+
+//     return (
+//       pronoun[proIndex] + adj[adjIndex] + noun[nounIndex] + fin[finalIndex]
+//     );
+//   }
+//   document.querySelector("#boton").addEventListener("click", () => {
+//     document.querySelector("#dominios").innerHTML = dominios();
+//   });
+// };
